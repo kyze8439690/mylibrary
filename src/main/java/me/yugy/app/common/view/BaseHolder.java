a27752924d35ea34713942eb84d244fc4b444c28
Yugy <me@yanghui.name>
2015-07-26 15:38:05 +0800

    add BaseHolder

---
+++ src/main/java/me/yugy/app/common/view/BaseHolder.java
@@ -0,0 +1,18 @@
+package me.yugy.app.common.view;
+
+import android.view.View;
+
+import butterknife.ButterKnife;
+
+public abstract class BaseHolder<T> {
+
+    View rootView;
+
+    public BaseHolder(View view) {
+        rootView = view;
+        ButterKnife.inject(this, view);
+        view.setTag(this);
+    }
+
+    public abstract void parse(T data);
+}
