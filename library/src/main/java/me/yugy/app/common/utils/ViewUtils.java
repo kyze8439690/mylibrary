7cc3965bd597fec6422bcbf302c98b815eaa889e
Yugy <me@yanghui.name>
2015-12-01 00:29:24 +0800

    add ViewUtils.java

---
+++ library/src/main/java/me/yugy/app/common/utils/ViewUtils.java
@@ -0,0 +1,50 @@
+package me.yugy.app.common.utils;
+
+import android.view.View;
+import android.view.ViewGroup;
+
+@SuppressWarnings("unused")
+public class ViewUtils {
+
+    public static void dispatchStartTemporaryDetach(View view) {
+        if (view instanceof ViewGroup) {
+            ViewGroup viewGroup = (ViewGroup) view;
+            final int count = viewGroup.getChildCount();
+            for (int i = 0; i < count; i++) {
+                final View child = viewGroup.getChildAt(i);
+                dispatchStartTemporaryDetach(child);
+            }
+        } else {
+            view.onStartTemporaryDetach();
+        }
+    }
+
+    public static void dispatchFinishTemporaryDetach(View view) {
+        if (view instanceof ViewGroup) {
+            ViewGroup viewGroup = (ViewGroup) view;
+            final int count = viewGroup.getChildCount();
+            for (int i = 0; i < count; i++) {
+                final View child = viewGroup.getChildAt(i);
+                dispatchFinishTemporaryDetach(child);
+            }
+        } else {
+            view.onFinishTemporaryDetach();
+        }
+    }
+
+    public static void offsetChildrenTopAndBottom(ViewGroup viewGroup, int offset) {
+        final int count = viewGroup.getChildCount();
+        for (int i = 0; i < count; i++) {
+            final View child = viewGroup.getChildAt(i);
+            child.offsetTopAndBottom(offset);
+        }
+    }
+
+    public static void offsetChildrenLeftAndRight(ViewGroup viewGroup, int offset) {
+        final int count = viewGroup.getChildCount();
+        for (int i = 0; i < count; i++) {
+            final View child = viewGroup.getChildAt(i);
+            child.offsetLeftAndRight(offset);
+        }
+    }
+}
