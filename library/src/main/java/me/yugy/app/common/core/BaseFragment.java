7d46eb9319a466354a7dd3898620cab6bf8e7240
Yugy <me@yanghui.name>
2016-02-13 21:55:49 +0800

    add getLayoutId() in BaseFragment

--- library/src/main/java/me/yugy/app/common/core/BaseFragment.java
+++ library/src/main/java/me/yugy/app/common/core/BaseFragment.java
@@ -1,9 +1,15 @@
 package me.yugy.app.common.core;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import com.android.volley.Request;
 
+import butterknife.ButterKnife;
 import me.yugy.app.common.network.RequestManager;
 
 @SuppressWarnings("unused")
@@ -23,6 +29,22 @@ public class BaseFragment extends Fragment {
 
     protected void onRealResume() {
 
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        if (getLayoutId() == 0) {
+            return super.onCreateView(inflater, container, savedInstanceState);
+        } else {
+            View rootView = inflater.inflate(getLayoutId(), container, false);
+            ButterKnife.inject(this, rootView);
+            return rootView;
+        }
+    }
+
+    public int getLayoutId() {
+        return 0;
     }
 
     public void addRequest(Request request) {
