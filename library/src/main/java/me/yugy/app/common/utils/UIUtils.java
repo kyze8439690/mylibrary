5affa036fd9d587511fec3da1275d1408b3e474b
Yugy <me@yanghui.name>
2016-02-17 00:21:31 +0800

    fix UIUtils bugs

--- library/src/main/java/me/yugy/app/common/utils/UIUtils.java
+++ library/src/main/java/me/yugy/app/common/utils/UIUtils.java
@@ -69,9 +69,9 @@ public class UIUtils {
         }
     }
 
-    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     public static void makeStatusBarTransparent(Window window) {
-        if (VersionUtils.JellyBeanOrLater()) {
+        if (VersionUtils.lollipopOrLater()) {
             window.getDecorView().setSystemUiVisibility(
                     View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
         }
