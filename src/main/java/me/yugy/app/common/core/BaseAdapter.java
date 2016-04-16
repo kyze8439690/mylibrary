96f16365b65567ce93415c96e8c07e1c07e15762
YangHui <me@yanghui.name>
2016-04-16 21:28:41 +0800

    update uil version

--- src/main/java/me/yugy/app/common/core/BaseAdapter.java
+++ src/main/java/me/yugy/app/common/core/BaseAdapter.java
@@ -1,6 +1,5 @@
 package me.yugy.app.common.core;
 
-import android.support.annotation.LayoutRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -9,6 +8,7 @@ import android.view.ViewGroup;
 
 import java.util.ArrayList;
 
+@SuppressWarnings("unused")
 public abstract class BaseAdapter<T, E extends BaseHolder<T>> extends android.widget.BaseAdapter {
 
     @NonNull private ArrayList<T> mData = new ArrayList<>();
