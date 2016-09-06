3005be743e73535f803661a90c0ddc1fb4e99fbc
Yugy <me@yanghui.name>
2016-09-06 20:44:46 +0800

    update AESUtils.java implement

--- src/main/java/me/yugy/app/common/utils/AESUtils.java
+++ src/main/java/me/yugy/app/common/utils/AESUtils.java
@@ -4,47 +4,79 @@ import android.support.annotation.Nullable;
 
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
+import java.security.Key;
 import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
 
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.KeyGenerator;
 import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.PBEKeySpec;
 import javax.crypto.spec.SecretKeySpec;
 
 @SuppressWarnings("unused")
 public class AESUtils {
 
-//    String key = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
-//    byte[] encrypt = AESUtils.AES_CFB_ENCRYPT(
-//            "fuckyoufuckyoufuckyoufuckyoufuckyoufuckyou".getBytes(),
-//            key, new IvParameterSpec("b1d15254f0f0417d".getBytes()));
-//    String decrypt = Base64.encodeToString(encrypt, Base64.DEFAULT);
-//    DebugUtils.log(decrypt);
-
     @Nullable
-    public static byte[] AES_CFB_ENCRYPT(byte[] source, String key, IvParameterSpec ivParam) {
+    public static byte[] generateKey(String password) {
         try {
-            Cipher cipher = Cipher.getInstance("AES/CFB/NoPadding");
-            cipher.init(Cipher.ENCRYPT_MODE, new SecretKeySpec(key.getBytes(), "AES"), ivParam);
-            return cipher.doFinal(source);
-        } catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException
-                | BadPaddingException | IllegalBlockSizeException | InvalidAlgorithmParameterException e) {
+            /* Store these things on disk used to derive key later: */
+            int iterationCount = 1000;
+            int saltLength = 32; // bytes; should be the same size as the output (256 / 8 = 32)
+            int keyLength = 256; // 256-bits for AES-256, 128-bits for AES-128, etc
+
+            /* When first creating the key, obtain a salt with this: */
+            SecureRandom random = new SecureRandom();
+            byte[] salt = new byte[saltLength];
+            random.nextBytes(salt);
+
+            /* Use this to derive the key from the password: */
+            KeySpec keySpec = new PBEKeySpec(password.toCharArray(), salt,
+                    iterationCount, keyLength);
+            SecretKeyFactory keyFactory = SecretKeyFactory
+                    .getInstance("PBKDF2WithHmacSHA1");
+            return keyFactory.generateSecret(keySpec).getEncoded();
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
             e.printStackTrace();
             return null;
         }
     }
 
     @Nullable
-    public static byte[] AES_CFB_DECRYPT(byte[] source, String key, IvParameterSpec ivParam) {
+    public static byte[] AES_CFB_ENCRYPT(byte[] source, byte[] key) {
         try {
-            Cipher cipher = Cipher.getInstance("AES/CFB/NoPadding");
-            cipher.init(Cipher.DECRYPT_MODE, new SecretKeySpec(key.getBytes(), "AES"), ivParam);
+            Cipher cipher = Cipher.getInstance("AES/CFB/PKCS5Padding");
+            IvParameterSpec iv = new IvParameterSpec(
+                    new byte[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,});
+            cipher.init(Cipher.ENCRYPT_MODE, new SecretKeySpec(key, "AES"), iv);
             return cipher.doFinal(source);
         } catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException
-                | BadPaddingException | IllegalBlockSizeException | InvalidAlgorithmParameterException e) {
+                | BadPaddingException | IllegalBlockSizeException
+                | InvalidAlgorithmParameterException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    @Nullable
+    public static byte[] AES_CFB_DECRYPT(byte[] source, byte[] key) {
+        try {
+            Cipher cipher = Cipher.getInstance("AES/CFB/PKCS5Padding");
+            IvParameterSpec iv = new IvParameterSpec(
+                    new byte[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,});
+            cipher.init(Cipher.DECRYPT_MODE, new SecretKeySpec(key, "AES"), iv);
+            return cipher.doFinal(source);
+        } catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException
+                | BadPaddingException | IllegalBlockSizeException
+                | InvalidAlgorithmParameterException e) {
             e.printStackTrace();
             return null;
         }
