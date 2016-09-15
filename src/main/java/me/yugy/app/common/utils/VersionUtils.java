c087b97a2a620ebc78dba6817da2b37e6d8e5fc0
YangHui <me@yanghui.name>
2016-09-16 01:52:12 +0800

    update VersionUtils.java

--- src/main/java/me/yugy/app/common/utils/VersionUtils.java
+++ src/main/java/me/yugy/app/common/utils/VersionUtils.java
@@ -27,6 +27,13 @@ public class VersionUtils {
     }
 
     /**
+     * Return {@code true} if api level >= 15.
+     */
+    public static boolean iceCreamMR1OrLater() {
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1;
+    }
+
+    /**
      * Return {@code true} if api level >= 16.
      */
     public static boolean jellyBeanOrLater() {
