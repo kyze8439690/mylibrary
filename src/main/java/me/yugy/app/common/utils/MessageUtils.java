51bff934b8b2b659077d6100b0eec48c40cefd66
YangHui <me@yanghui.name>
2016-09-17 01:35:12 +0800

    add snackbar support

--- src/main/java/me/yugy/app/common/utils/MessageUtils.java
+++ src/main/java/me/yugy/app/common/utils/MessageUtils.java
@@ -2,19 +2,29 @@ package me.yugy.app.common.utils;
 
 import android.content.Context;
 import android.os.Looper;
+import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
+import android.view.ViewGroup;
 import android.widget.Toast;
 
 @SuppressWarnings("unused")
 public class MessageUtils {
 
-    public static void toast(Context context, String text){
+    public static void toast(Context context, String text) {
         Toast.makeText(context, text, Toast.LENGTH_SHORT).show();
     }
 
-    public static void toastOnNonUIThread(Context context, String text){
+    public static void toastOnNonUIThread(Context context, String text) {
         Looper.prepare();
         Toast.makeText(context, text, Toast.LENGTH_SHORT).show();
         Looper.loop();
         Looper.myLooper().quit();
+    }
+
+    public static void snack(@Nullable ViewGroup container, String text) {
+        if (container == null) {
+            return;
+        }
+        Snackbar.make(container, text, Snackbar.LENGTH_SHORT).show();
     }
 }
